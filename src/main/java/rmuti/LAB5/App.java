1cc202e8b7676db5eebd6d647e9accaa45b53f22
wanmongkon <wanmongkon.ke@kw.ac.th>
2018-03-14 14:19:44 +0700

    LAB5

---
+++ src/main/java/rmuti/LAB5/App.java
@@ -0,0 +1,13 @@
+package rmuti.LAB5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/rmuti/LAB5/Employee.java
@@ -0,0 +1,31 @@
+package rmuti.lab05;
+
+import rmuti.lab05.Employee;
+
+public class Employee implements Comparable {
+	private String empId;
+	private String name;
+	private int salary;
+
+	
+	public Employee(String empId, String name, int salary) {
+		this.empId = empId;
+		this.name = name;
+		this.salary = salary;	
+	}
+	@Override
+	public String toString() {
+		return "{empId = "+empId+", name = "+name+",salary ="+salary+"}";
+	}
+	
+	public int compareTo(Object o) {
+		Employee otherEmp = (Employee) o;
+		if (this.salary > otherEmp.salary) {
+			return 1;
+		}else if (this.salary < otherEmp.salary) {
+			return -1;
+		}
+		return 0;
+	}
+	
+}
